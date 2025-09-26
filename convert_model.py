2f30541c309c2507875cae94c45cfe93724f227f
Naajis Siddiqui <naajissiddiqui123@gmail.com>
2025-09-26 22:52:11 +0530

    completed project

--- convert_model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-# Load using tf.keras instead of keras
-old_model = tf.keras.models.load_model("models/model.h5", compile=False)
-
-# Save in new format
-old_model.save("models/model.keras", save_format="keras")
-
-print("✅ Model successfully converted to model.keras")
